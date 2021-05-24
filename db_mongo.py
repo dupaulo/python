8cbafba0ecb50d7ac3f87d29dd48bee0e0062478
Paulo Eduardo Heitor Silva <32689349+dupaulo@users.noreply.github.com>
2021-05-24 14:19:26 -0300

    Função adicionada
    Função que realiza a padronização do arquivo CSV para JSON.

--- db_mongo.py
+++ db_mongo.py
@@ -8,9 +8,17 @@ client = MongoClient()
 client = MongoClient(host="localhost", port=27017)
 db = client.db_aprendendo
 brds = db['brds']
-      
 
-with open ('arquivos/brdsPatrocinados.json') as f:
+with open('arquivos/brdsPatrocinados1.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+
+#crio arquivo Jason para ser utilizado para carga no banco de dados
+with open('arquivos/MeuArquivoJson.json', 'w') as f:
+    json.dump(rows, f)
+
+#Abro o arquivo criado faço insert na linha 23
+with open ('arquivos/MeuArquivoJson.json') as f:
     file_data = json.load(f)
 brds.insert(file_data)
 
